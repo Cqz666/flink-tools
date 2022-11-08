39f6121f0099e22cedf7c02be76765dba4612175
chenqizhu <chenqizhu@4399inc.com>
2022-11-08 10:30:49 +0800

    test PR

--- src/test/java/com/cqz/flink/datagen/TestPR.java
+++ src/test/java/com/cqz/flink/datagen/TestPR.java
@@ -3,5 +3,6 @@ package com.cqz.flink.datagen;
 public class TestPR {
     public static void main(String[] args) {
         System.out.println("TEST PR");
+        System.out.println("TEST PR");
     }
 }
